da5654dbf0bcb01dbcde22d935c2b1881ad87f6c
Leo Koo <leokoo@gmail.com>
2015-11-23 02:23:27 +0800

    more updates

--- db/migrate/20151121101512_merge_names.rb
+++ db/migrate/20151121101512_merge_names.rb
@@ -1,11 +1,13 @@
 require_relative '../config'
+require 'byebug'
 
 class MergeNames < ActiveRecord::Migration
 	def up
 		add_column :students, :name, :string
 
-		Student.all.each do |person|
-		person.update_attributes! :name => person.first_name + "" + person.last_name
+	        Student.all.each do |x|
+          x.update(name: "#{x.first_name} #{x.last_name}")
+        end
 		end
 		remove_column :students, :first_name
 		remove_column :students, :last_name
@@ -16,8 +18,8 @@ class MergeNames < ActiveRecord::Migration
 		add_column :students, :last_name, :string
 
 		Student.all.each do |person|
-			person.update_attributes! :first_name => person.name.match(/\w+/)[0]
-			person.update_attributes! :last_name => person.name.match(/\w+/)[1]
+			person.update_attributes :first_name => person.name.match(/\w+/)[0]
+			person.update_attributes :last_name => person.name.match(/\w+/)[1]
 		end
 		remove_column :students, :name
 	end
