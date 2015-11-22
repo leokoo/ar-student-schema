9d21f24e328f2f0767bd5446d15437e0c4e245b9
Leo Koo <leokoo@gmail.com>
2015-11-23 02:10:25 +0800

    fixed errors with migrate

--- db/migrate/20151121101512_merge_names.rb
+++ db/migrate/20151121101512_merge_names.rb
@@ -4,21 +4,21 @@ class MergeNames < ActiveRecord::Migration
 	def up
 		add_column :students, :name, :string
 
-		MergeNames.all.each do |person|
+		Student.all.each do |person|
 		person.update_attributes! :name => person.first_name + "" + person.last_name
 		end
-
-		remove_column: students, :first_name
-		remove_column: students, :last_name
+		remove_column :students, :first_name
+		remove_column :students, :last_name
+	end
 
 	def down
 		add_column :students, :first_name, :string
 		add_column :students, :last_name, :string
 
-		MergeNames.all.each do |person|
+		Student.all.each do |person|
 			person.update_attributes! :first_name => person.name.match(/\w+/)[0]
 			person.update_attributes! :last_name => person.name.match(/\w+/)[1]
 		end
-
-		remove_column: students, :name
+		remove_column :students, :name
+	end
 end
